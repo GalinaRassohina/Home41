7d0508ae5f6b1a671160bb2b37155a9b256a0bcf
Galina <gal-rassohina@yandex.ru>
2022-11-27 11:57:57 +0700

    исправила ошибки

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿// Напишите метод, который принимает на вход два числа (A и B) и возводит 
 //число A в натуральную степень B.3, 5 -> 243 (3⁵) 2, 4 -> 16
 // В методе цикл, который умножает А само на себя В раз
-void PowerNumber (int a = 3, int b = 5);
+void PowerNumber (int a , int b)
 {
-int result = 1;
-for( int i = 1; i < b; i++) 
-{
+ int result = 1;
+  for( int i = 1; i < b; i++) 
+ {
     result =result * a;
+ }
+ Console.WritLine(result);
 }
-Console.WritLine($"{a}^{b } = "{result});
-}
+PowerNumber(3, 5);
+
+//Console.WritLine($"{a}^{b } = {result}");
+
 
